6d1f10b71abdc1e21d4bf673b6bd4dd11b2bf332
leonel <leonell_ceolin@hotmail.com>
2020-01-30 15:18:00 -0400

    check duplicate email

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,7 +3,5 @@ import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
-
-routes.post('users', UserController.store);
+routes.post('/users', UserController.store);
 export default routes;
