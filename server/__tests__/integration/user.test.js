4fd5b1ff56e2fb9f75dc520f6f5ff00faf039b09
leonel <leonell_ceolin@hotmail.com>
2020-01-30 15:39:57 -0400

    generate random data

--- server/__tests__/integration/user.test.js
+++ server/__tests__/integration/user.test.js
@@ -1,5 +1,9 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
+
+// import User from '../../src/app/models/User';
+import factory from '../factories';
 import truncate from '../util/truncate';
 
 describe('User', () => {
@@ -7,34 +11,34 @@ describe('User', () => {
     await truncate();
   });
 
+  it('should encrypt user password when new user created', async () => {
+    const user = await factory.create('User', {
+      password: '12346',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Leonel Ceolin Farias',
-        email: 'leonell_ceolin@hotmail.com',
-        password_hash: '12346',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
     await request(app)
       .post('/users')
-      .send({
-        name: 'Leonel Ceolin Farias',
-        email: 'leonell_ceolin@hotmail.com',
-        password_hash: '12346',
-      });
+      .send(user);
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Leonel Ceolin Farias',
-        email: 'leonell_ceolin@hotmail.com',
-        password_hash: '12346',
-      });
+      .send(user);
 
     expect(response.status).toBe(400);
   });
